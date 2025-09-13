b5afdbfd3acd5333c3034959764da0c1080b0721
Hrishi Shah <114273264+Hrishirt@users.noreply.github.com>
2025-09-13 02:56:34 -0600

    Bug fix for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import cors from "cors"
 import {app, io, server} from "./lib/socket.js"
 import path from "path"
 dotenv.config()
-
+const __dirname = path.resolve();
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
@@ -20,6 +20,8 @@ app.use(cors({
 app.use("/api/auth", AuthRoutes)
 app.use("/api/messages", messageRoutes)
 
+
+
 if(process.env.NODE_ENV ==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -29,7 +31,6 @@ if(process.env.NODE_ENV ==="production"){
 }
 
 const PORT = process.env.PORT
-const __dirname = path.resolve();
 
 server.listen(PORT, ()=> {
     console.log(`Server is running on PORT ${PORT}`)
